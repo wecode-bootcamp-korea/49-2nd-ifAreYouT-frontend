2dec15f9af4bc8e62784310712c533bdc3ecdafc
imjueun <leeje7766@gmail.com>
2023-10-11 19:44:58 +0900

    초기세팅 3차 수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
-import Login from './pages/Users/Login/Login';
+import Login from './pages/Login/Login';
 import SignUp from './pages/Users/SignUp/SignUp';
 import ProductList from './pages/ProductList/ProductList';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
@@ -12,9 +12,9 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/productlist" element={<ProductList />} />
-        <Route path="/productdetail" element={<ProductDetail />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/product-list" element={<ProductList />} />
+        <Route path="/product-detail" element={<ProductDetail />} />
         <Route path="/order" element={<Order />} />
       </Routes>
     </BrowserRouter>
