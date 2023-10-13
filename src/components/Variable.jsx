26e265f04220a9ea4364cea37f8d0733b5e3acb5
jyj <dudwns5013@naver.com>
2023-10-13 16:28:51 +0900

    [feat] 좌석 UI 및 클릭시 색 변함

--- src/components/Variable.jsx
+++ src/components/Variable.jsx
@@ -1,35 +1,35 @@
-const details = [
-  {
-    grade: 'A',
-    price: 180000,
-  },
-  {
-    grade: 'B',
-    price: 160000,
-  },
-  {
-    grade: 'C',
-    price: 140000,
-  },
-  {
-    grade: 'D',
-    price: 120000,
-  },
-];
-
-const seats = (() => {
-  const seat = [];
-  for (let i = 0; i < 20; i++) {
-    for (let j = 1; j <= 30; j++) {
-      seat.push({
-        seatId: j + i * 30,
-        name: `${String.fromCharCode(i + 65)}-${j}`,
-        status: true,
-        grade: details[Math.floor((j + i * 30) / 150)],
-      });
+export const seatMockData = (() => {
+  const details = [
+    {
+      grade: 'A',
+      price: 180000,
+    },
+    {
+      grade: 'B',
+      price: 160000,
+    },
+    {
+      grade: 'C',
+      price: 140000,
+    },
+    {
+      grade: 'D',
+      price: 120000,
+    },
+  ];
+  const seats = (() => {
+    const seat = [];
+    for (let i = 0; i < 20; i++) {
+      for (let j = 1; j <= 30; j++) {
+        seat.push({
+          seatId: j + i * 30,
+          name: `${String.fromCharCode(i + 65)}-${j}`,
+          status: true,
+          seatGrade: details[Math.ceil((i * 30 + j) / 150) - 1],
+        });
+      }
     }
-  }
-  return seat;
+    return seat;
+  })();
+  return { details, seats };
 })();
-
-export const seatMockData = { details, seats };
--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -4,9 +4,16 @@ import './Order.scss';
 
 const Order = () => {
   const [seatData, setSeatData] = useState([]);
+  const [selected, setSelected] = useState(Array(600).fill(false));
   useEffect(() => {
     setSeatData(seatMockData.seats);
   }, []);
+
+  const handleClick = idx => {
+    const buttonState = [...selected];
+    buttonState[idx] = !buttonState[idx];
+    setSelected(buttonState);
+  };
 
   if (seatData.length === 0) {
     return null;
@@ -15,10 +22,62 @@ const Order = () => {
   return (
     <div className="order">
       <div className="orderContainer">
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
+        <div className="orderHeader">주문/좌석 선택</div>
+        <div className="productInfo">장소 : 테헤란로</div>
+        <div className="gradeInfo" />
+        <div className="concertHall">
+          <div className="stage">
+            <div>STAGE</div>
+          </div>
+          <div className="seatTable">
+            <div />
+            <div className="seatColumn">
+              {Array(30)
+                .fill(1)
+                .map((val, idx) => {
+                  return (
+                    <div className="column" key={idx}>
+                      {val + idx}
+                    </div>
+                  );
+                })}
+            </div>
+            <div className="seatRow">
+              {Array(20)
+                .fill(1)
+                .map((val, idx) => {
+                  return (
+                    <div className="row" key={idx}>
+                      {String.fromCharCode(val + idx + 64)}
+                    </div>
+                  );
+                })}
+            </div>
+            <div className="seatList">
+              {seatData.map((seatInfo, idx) => {
+                const { seatId, status, seatGrade } = seatInfo;
+                const { grade } = seatGrade;
+                const selectColor = selected[idx] ? '#e50815' : '';
+
+                return (
+                  <input
+                    className={`seat ${grade}`}
+                    key={seatId}
+                    type="Button"
+                    onClick={() => handleClick(idx)}
+                    style={{
+                      backgroundColor: `${status ? selectColor : 'black'}`,
+                    }}
+                    disabled={!status}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <div className="paymentInfo" />
+        <div className="orderFooter">주문 / 좌석 선택</div>
+        <div className="orderFooter">결제 단계</div>
       </div>
     </div>
   );
