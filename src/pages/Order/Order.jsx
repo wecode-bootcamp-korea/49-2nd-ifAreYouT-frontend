5473282d7b65b363d8bd9637cb4c760c62416b17
jyj <dudwns5013@naver.com>
2023-10-13 12:53:54 +0900

    [feat] 상수 데이터 생성

--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -1,5 +1,27 @@
+import { useState, useEffect } from 'react';
+import { seatMockData } from '../../components/Variable';
+import './Order.scss';
+
 const Order = () => {
-  return <div>Order</div>;
+  const [seatData, setSeatData] = useState([]);
+  useEffect(() => {
+    setSeatData(seatMockData.seats);
+  }, []);
+
+  if (seatData.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className="order">
+      <div className="orderContainer">
+        <div>a</div>
+        <div>a</div>
+        <div>a</div>
+        <div>a</div>
+      </div>
+    </div>
+  );
 };
 
 export default Order;
