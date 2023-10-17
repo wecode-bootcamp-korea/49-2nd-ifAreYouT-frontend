3531d1ee154382068e1c102e89e63c4578df3363
imjueun <leeje7766@gmail.com>
2023-10-17 22:26:06 +0900

    [feat] 상세페이지 작업중

---
+++ src/pages/ProductDetail/ProductDetailTop/DetailTop.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from 'react';
+import DetailTop from '../DetailTop.scss';
+
+const DetailTop = () => {
+  const [detailTop, setDetailTopData] = useState([]);
+  return <div>ProductList</div>;
+};
+
+export default DetailTop;
