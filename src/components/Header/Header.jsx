ba51603726c4fb61a73ebdfbdbfd5642d0733d30
jyj <dudwns5013@naver.com>
2023-10-23 16:46:36 +0900

    [feat] 초기

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,15 @@
+import './Header.scss';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <img src="/images/logo.png" alt="logo" className="logo" />
+      <div>발라드</div>
+      <div>힙합</div>
+      <div>재즈</div>
+      <div>아이돌</div>
+    </header>
+  );
 };
 
 export default Header;
